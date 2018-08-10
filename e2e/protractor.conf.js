bbd24bd9a42c705b869bccc7809cff5326cb1170
Todd Palmer <tpalmer@us.fujitsu.com>
2018-08-09 23:15:19 -0400

    e2e

--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -9,6 +9,9 @@ exports.config = {
     './src/**/*.e2e-spec.ts'
   ],
   capabilities: {
+    chromeOptions: {
+      args: [ "--headless", "--disable-gpu", "--window-size=800x600" ]
+    },
     'browserName': 'chrome'
   },
   directConnect: true,
